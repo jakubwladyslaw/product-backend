5e2a611869be17d7aaa0391060128ce8cfe3d6f3
Jakub Żurawa <jakub.zurawa@gmail.com>
2018-07-12 20:46:20 +0200

    Fix migrations path

--- src/ProductBackendServiceProvider.php
+++ src/ProductBackendServiceProvider.php
@@ -13,7 +13,7 @@ class ProductBackendServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
     }
 
